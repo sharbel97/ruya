dec7b1c37dea6ced90d46ba06cfb11876938faa1
sharbel <sharbelhoma@hotmail.com>
2021-12-11 17:51:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import cv2
+
+# Access video device/webcam
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Getting frames from the webcam
+    retrieve, frames = camera.read()
+    
+    # Setting up the frames into a UI window
+    cv2.imshow('self', frames)
+
+    # Closing the window on 'q' click
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break;
+        
