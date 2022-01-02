0eb1a9044068815d915123d6b7c8300b5037bc9c
sharbel <sharbelhoma@hotmail.com>
2022-01-02 15:14:32 -0600

    updated file structure

---
+++ image.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+// openCV libraries
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+using namespace cv;
+
+int main(int argc, const** char argv) {
+    return 0;
+}
