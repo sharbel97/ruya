74d72ba020d1d1e7f8039f9401adb5bd3e087ae7
sharbel <sharbelhoma@hotmail.com>
2022-01-02 19:11:23 -0600

    use webcam

--- ruya/ruya/main.cpp
+++ ruya/ruya/main.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 // OpenCV includes
-#include "opencv2/core.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/highgui.hpp"
 using namespace cv;
 
@@ -19,17 +19,49 @@ using namespace cv;
 int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     
-    Mat color = imread("/Users/sharbster/Desktop/resources/car.jpg");
+    const char* keys =
+    {
+        "{help h usage ? | | print this message}"
+        "{@video | | Video file, if not defined try to use webcamera}"
+    };
     
-    if (!color.data) {
-        cout << "Could not open or find image" << endl;
-        return -1;
+    
+    CommandLineParser parser(argc, argv, keys);
+    
+    //If requires help show
+    if (parser.has("help")) {
+        parser.printMessage();
+        return 0;
     }
     
-    // show images
-    imshow("Benz Color", color);
+    String videoFile= parser.get<String>(0);
     
-    // wait for any key press
-    waitKey(0);
+    // Check if params are correctly parsed in his variables
+    if (!parser.check()) {
+    parser.printErrors();
+    return 0;
+    }
+    
+    // open the default camera
+    VideoCapture cap;
+    if(videoFile != "")
+        cap.open(videoFile);
+    else
+        cap.open(0);
+    
+    if(!cap.isOpened()) // check if we succeeded
+        return -1;
+    
+    namedWindow("Video",1);
+    for(;;) {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        if(frame.data)
+            imshow("Video", frame);
+        if(waitKey(30) >= 0) break;
+    }
+    
+    // Release the camera or video cap
+    cap.release();
     return 0;
 }
