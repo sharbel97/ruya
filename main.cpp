b1011bc2885e5e0d45bf1f0af4c686e7bb611519
sharbel <sharbelhoma@hotmail.com>
2022-01-03 18:22:52 -0600

    updates

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  ruya
+//
+//  Created by Sharbel on 1/2/22.
+//
+
+#include <iostream>
+using namespace std;
+
+// OpenCV includes
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+using namespace cv;
+
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    
+    return 0;
+}
